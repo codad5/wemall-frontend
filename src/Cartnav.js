964cbfbbb12245024bb6a10b0a27eb7cc4bed2f2
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-04-05 01:18:15 +0100

    Cart NAv development

--- src/Cartnav.js
+++ src/Cartnav.js
@@ -1,9 +1,34 @@
 import react from 'react';
+import { Link } from 'react-router-dom'
 
 export default function Cartnav(){
     return (
-        <aside>
-            
+        <aside className="cart-container">
+            <div className="cart-header">
+                Cart
+
+            </div>
+            <div className="cart-item-body">
+                <div className="cart-item-box">
+                    <div className="cart-productImage"></div>
+                    <div className="cart-item-description">
+                        <div className="cart-product-name">
+                            Joes
+                        </div>
+                        <div className="cart-item-prices">
+                            <span>$100</span> <span className="cart-item-total">$1000</span>
+                        </div>
+                        <div className="cart-item-action-btns">
+                            <Link to={{
+                                pathname:'../view/hiagdfiub'
+                            }}>Edit</Link>
+                            <button type="submit">
+                                Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </aside>
     )
 }
--- src/Header.js
+++ src/Header.js
@@ -77,6 +77,7 @@ export default function Header(props){
                 </div>
 
             </div>
+            <Cartnav></Cartnav>
         </header>
     )
 }
