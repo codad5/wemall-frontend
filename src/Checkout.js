a0c343bd94933dafb24be69580160d23eadbe292
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-05-03 02:26:39 +0100

    debugging

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Signup, Login} from './Login'
 import { usePaystackPayment } from "react-paystack"
 import Header from "./Header"
